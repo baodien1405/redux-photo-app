5dbd6529d531f41a2e8b25c864a36a0d05504b14
Dien Cap <baodien1405@gmail.com>
2021-03-26 11:53:01 +0700

    Initial commit

---
+++ src/features/Photo/pages/Main/index.jsx
@@ -0,0 +1,53 @@
+import Banner from "components/Banner";
+import Images from "constants/images";
+import PhotoList from "features/Photo/components/PhotoList";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import { Container } from "reactstrap";
+import { removePhoto } from "features/Photo/photoSlice";
+
+// import Banner from "../../../../components/Banner";
+// import Images from "../../../../constants/images";
+
+MainPage.propTypes = {};
+
+function MainPage(props) {
+  const dispatch = useDispatch();
+  const photos = useSelector((state) => state.photos);
+  const history = useHistory();
+  console.log("List of photos: ", photos);
+
+  const handlePhotoEditClick = (photo) => {
+    console.log("Edit: ", photo);
+    const editPhotoUrl = `/photos/${photo.id}`;
+    history.push(editPhotoUrl);
+  };
+
+  const handlePhotoRemoveClick = (photo) => {
+    console.log("Remove: ", photo);
+    const removePhotoId = photo.id;
+    const action = removePhoto(removePhotoId);
+    dispatch(action);
+  };
+
+  return (
+    <div className="photo-main">
+      <Banner title="Your awesome photo" backgroundUrl={Images.PINK_BG} />
+
+      <Container className="text-center">
+        <div className="py-5">
+          <Link to="/photos/add">Add new photo</Link>
+        </div>
+
+        <PhotoList
+          photoList={photos}
+          onPhotoEditClick={handlePhotoEditClick}
+          onPhotoRemoveClick={handlePhotoRemoveClick}
+        />
+      </Container>
+    </div>
+  );
+}
+
+export default MainPage;
